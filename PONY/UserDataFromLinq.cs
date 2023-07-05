e03c6bca272cb81563e24df0590b3e305db16d74
pony0278 <steam2744@gmail.com>
2023-07-05 22:32:54 +0800

    no message

--- PONY/UserDataFromLinq.cs
+++ PONY/UserDataFromLinq.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CatCaha.NewFolder1;
+using CatChaForms;
 
 namespace CatCaha.UserDataSouce
 {
@@ -13,8 +14,9 @@ namespace CatCaha.UserDataSouce
         project123Entities cat = new project123Entities();
         public List<UserData> GetUserDatas()
         {
+            var ID = LoggedInUser.ID;
             return cat.Shop_Member_Info
-                .Where(u => u.Member_ID == 1)
+                .Where(u => u.Member_ID == ID)
                 .Select(u => new UserData
                 {
                     Name = u.Name,
@@ -22,19 +24,19 @@ namespace CatCaha.UserDataSouce
                     LoyaltyPoints = (int)u.Loyalty_Points
                 }).ToList();
         }
-        public List<UserData> UserDataRefresh()
-        {
-            using(var u  = new project123Entities())
-            {
-                return u.Shop_Member_Info
-                    .Where(user => user.Member_ID == 1)
-                    .Select(user => new UserData
-                    {
-                        Name = user.Name,
-                        catcoin = (int)user.Cat_Coin_Quantity,
-                        LoyaltyPoints = (int)user.Loyalty_Points
-                    }).ToList();
-            }
-        }
+        //public List<UserData> UserDataRefresh()
+        //{
+        //    using(var u  = new project123Entities())
+        //    {
+        //        return u.Shop_Member_Info
+        //            .Where(user => user.Member_ID == ID)
+        //            .Select(user => new UserData
+        //            {
+        //                Name = user.Name,
+        //                catcoin = (int)user.Cat_Coin_Quantity,
+        //                LoyaltyPoints = (int)user.Loyalty_Points
+        //            }).ToList();
+        //    }
+        //}
     }
 }
